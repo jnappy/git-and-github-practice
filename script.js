2902918ff399a0e58398fa1cfc4eee23f44bfe94
Jeff Boykin <jeff.boykin@generalassemb.ly>
2015-11-12 12:00:45 -0500

    Added Button for Jay

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
     $("#button").click(function(){
-    	event.preventDefault(); 
+    	event.preventDefault();
       	// add a line here that alerts the user "I've been clicked"
-    }); 
+        alert("I've been clicked")
+    });
 });
